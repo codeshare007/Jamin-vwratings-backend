bba3bbd8d0e4061b8f238a6f812d8558e8fe9893
robertj1029 <robertj9216@gmail.com>
2021-11-08 07:51:53 +0000

    added info from old site... needs style

--- resources/js/views/buttons/peepscreeps.vue
+++ resources/js/views/buttons/peepscreeps.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="peepscreeps-page">
-
+                                <p class="mb-4">
+                                   Nominate and vote on your favorite players and the worst players you know.<br>Peeps will get treats.<br>Creeps are just creeps.
+                                </p>
+                                <h2 class="mb-4">
+                                  COMING SOON 
+                                </h2>								
+                                <div class="sidey text-center">
+                                    <a href="" class="btn btn-primary tm-intro-btn tm-page-link">
+                                        Peeps
+                                    </a><br>
+									Peeps = Decent players
+                                </div>
+                                <div class="sidey text-center">
+                                    <a href="" class="btn btn-primary tm-intro-btn tm-page-link">
+                                        Creeps
+                                    </a><br>
+									Creeps = Not so decent
+                                </div>
   </div>
 </template>
 <script>
